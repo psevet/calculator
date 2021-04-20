c2bc8aa914839485a640ba2be0b69e641d8690db
Rostyslav Lekh <rostlex2001@gmail.com>
2021-04-20 15:06:27 +0300

    1.2

--- calc.js
+++ calc.js
@@ -1,50 +1,30 @@
-"use strict";
+(function () {
+   "use strict";
 
-let plus = document.getElementById("plus");
-let minus = document.getElementById("minus");
-let multiply = document.getElementById("multiply");
-let divide = document.getElementById("divide");
-let remainder = document.getElementById("remainder");
-let num1 = document.getElementById("num1");
-let num2 = document.getElementById("num2");
-let output = document.getElementById("output");
+   let plus = document.getElementById("plus");
+   let minus = document.getElementById("minus");
+   let multiply = document.getElementById("multiply");
+   let divide = document.getElementById("divide");
+   let remainder = document.getElementById("remainder");
+   let num1 = document.getElementById("num1");
+   let num2 = document.getElementById("num2");
+   let output = document.getElementById("output");
 
-plus.addEventListener("click", () => {
-   if (num1.value !== "" && num2.value !== "") {
-      output.innerHTML = parseFloat(num1.value) + parseFloat(num2.value);
-   } else {
-      alert("Enter second number");
-   }
-});
+   function calculate(operation) {
+      if (num1.value !== "" && num2.value !== "") {
+         output.innerHTML = (eval(`${num1.value} ${operation}  ${num2.value}`));
+      } else {
+         alert("Enter correct parameters");
+      }
+   };
 
-minus.addEventListener("click", () => {
-   if (num1.value !== "" && num2.value !== "") {
-      output.innerHTML = parseFloat(num1.value) - parseFloat(num2.value);
-   } else {
-      alert("Enter second number");
-   }
-});
+   plus.addEventListener("click", () => calculate('+'));
 
-multiply.addEventListener("click", () => {
-   if (num1.value !== "" && num2.value !== "") {
-      output.innerHTML = parseFloat(num1.value) * parseFloat(num2.value);
-   } else {
-      alert("Enter second number");
-   }
-});
+   minus.addEventListener("click", () => calculate('-'));
 
-divide.addEventListener("click", () => {
-   if (num1.value !== "" && num2.value !== "") {
-      output.innerHTML = parseFloat(num1.value) / parseFloat(num2.value);
-   } else {
-      alert("Enter second number");
-   }
-});
+   multiply.addEventListener("click", () => calculate('*'));
 
-remainder.addEventListener("click", () => {
-   if (num1.value !== "" && num2.value !== "") {
-      output.innerHTML = parseFloat(num1.value) % parseFloat(num2.value);
-   } else {
-      alert("Enter second number");
-   }
-});
+   divide.addEventListener("click", () => calculate('/'));
+
+   remainder.addEventListener("click", () => calculate('%'));
+})()
